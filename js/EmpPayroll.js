350b6a924d703551320db38699a52a3a9be44cc7
Arun <arungawande1995@gmail.com>
2022-05-03 22:45:51 +0530

    Added: Reset Option

--- js/EmpPayroll.js
+++ js/EmpPayroll.js
@@ -175,3 +175,35 @@ function createAndUpdateStorage(employeePayrollData) {
   }
   localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
 }
+
+
+
+
+const resetForm = () => {
+  setValue("#name", "");
+  unsetSelectedValues("[name=profile]");
+  unsetSelectedValues("[name=gender]");
+  unsetSelectedValues("[name=department]");
+  setValue("#salary", "");
+  setValue("#notes", "");
+  setValue("#day", "");
+  setValue("#month", "");
+  setValue("#year", "");
+};
+
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach((item) => {
+    item.checked = false;
+  });
+};
+
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContent = value;
+};
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+};
